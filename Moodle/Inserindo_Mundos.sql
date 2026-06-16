c095cb7aeccc60bb9ebba6c1fd9e4d9ef68d177c
Mayara Cristina <mayara.c@ges.inatel.br>
2026-06-16 09:16:25 -0300

    Create Inserindo_Mundos.sql

---
+++ Moodle/Inserindo_Mundos.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Mundo (
+	Id INT PRIMARY KEY,
+    Nome VARCHAR(45),
+    Descricao VARCHAR(100)
+);
+
+INSERT INTO Mundo VALUES
+(1,'Maygïk','Uma teocracia aliada aos magnatas exploradores regem esse mundo. Os desafios vão muito além do que você espera.'),
+(2,'Lucinera','O mundo é dominado por dragões. Como você vai trilhar o seu caminho:  Para livrar o seu povo ou para ascender como um senhor dos dragões?');
+
+SELECT * FROM Mundo;
